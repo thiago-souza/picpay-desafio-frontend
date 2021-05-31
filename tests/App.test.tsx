1f68f96808978a3863f84a043d270c74cdb14d68
Tiago (Montanha) <tiago.vilasboas@dextra-sw.com>
2021-05-31 19:00:55 -0300

    Setup initial boilerplate configuration in react

---
+++ tests/App.test.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { render } from "@testing-library/react";
+import App from "../src/presentation/components/App";
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+  });
+});
