925655628d0fcf281f2544a582c4eba36cdf7f33
Lucas Val <lucas.val@dextra-sw.com>
2021-06-29 18:07:58 -0300

    Remove test from app

--- src/pages/main/app.spec.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import App from './app';
-
-describe('App', () => {
-  test('renders App component', () => {
-    render(<App />);
-  });
-});
