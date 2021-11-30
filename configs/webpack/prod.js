40ed4054004e18921940e7f4605085ac8b4e62fd
Tiago Vilas Boas <tiago.vilasboas@dextra-sw.com>
2021-11-30 15:44:38 -0300

    Remoção da página do KYC dos mecanismos de busca

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -4,6 +4,7 @@ const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./common');
 const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -18,6 +19,9 @@ module.exports = merge(commonConfig, {
     new HtmlWebpackPlugin({
       template: './template-prod.html.ejs',
       favicon: './favicon-cartola.png',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/robots.txt', to: 'robots.txt' }],
     }),
     new Dotenv({
       path: './.env', // load this now instead of the ones in '.env'),
