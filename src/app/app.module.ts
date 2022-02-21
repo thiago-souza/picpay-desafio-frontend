f8352c11a0b3379ccc85cb00191cab1bda998413
Gizelly Silva <gizelly.silva@playkids.com>
2022-02-20 21:10:40 -0400

    feat: update structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [	
     AppComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
